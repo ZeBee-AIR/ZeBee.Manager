8d4262cd3889e34bb83c386522f4211a79f3922f
O-A-Mi <o.ou.a.mis@gmail.com>
2025-08-13 15:15:45 -0300

    Alter no Piechart para um piechart sem fill dentro

--- ZeBeeManagerFrontend/src/pages/Dashboard.tsx
+++ ZeBeeManagerFrontend/src/pages/Dashboard.tsx
@@ -285,7 +285,7 @@ const Dashboard = () => {
                         <CardContent>
                             <ResponsiveContainer width="100%" height={300}>
                                 <PieChart>
-                                    <Pie data={businessLogic.activeClientsBySquadData} cx="50%" cy="50%" labelLine={false} label={({ name, value }) => `${name}: ${value}`} outerRadius={80} fill="#8884d8" dataKey="value" >
+                                    <Pie data={businessLogic.activeClientsBySquadData} cx="50%" cy="50%" labelLine={false} label={({ name, value }) => `${name}: ${value}`} innerRadius={60} outerRadius={80} fill="#8884d8" dataKey="value" >
                                         {businessLogic.activeClientsBySquadData.map((entry, index) => (<Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />))}
                                     </Pie>
                                     <Tooltip formatter={(value: number) => `${value} cliente(s)`} />
